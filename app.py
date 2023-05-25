42cd36acdd296d8742929fd6cbdee329883a225e
kiel019 <202080497@psu.palawan.edu.ph>
2023-05-25 23:42:06 +0800

    Applied database connection to python file

--- app.py
+++ app.py
@@ -8,6 +8,12 @@ mysql = MySQL(app)
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_PASSWORD'] = 'apzx0dfd6'
-app.config['MYSQL_DB'] = 'mydatabase'
+app.config['MYSQL_DB'] = 'songlist'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
+@app.route('/')
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ songlist.sql
@@ -0,0 +1,32 @@
+CREATE table songs(
+	id int primary key auto_increment,
+    title varchar(255) not null,
+    artist varchar(255) not null,
+    album varchar(255),
+    release_year int not null
+);
+
+INSERT INTO songs (title, artist, album, release_year) 
+VALUES
+('Bohemian Rhapsody', 'Queen', 'A Night at the Opera', 1975),
+('Imagine', 'John Lennon', 'Imagine', 1971),
+('Like a Rolling Stone', 'Bob Dylan', 'Highway 61 Revisited', 1965),
+('Respect', 'Aretha Franklin', 'I Never Loved a Man the Way I Love You', 1967),
+("What's Going On", 'Marvin Gaye', "What's Going On", 1971),
+('Hey Jude', 'The Beatles', 'Abbey Road', 1969),
+('Stairway to Heaven', 'Led Zeppelin', 'Led Zeppelin IV', 1971),
+('Yesterday', 'The Beatles', 'Help!', 1965),
+('I Will Survive', 'Gloria Gaynor', 'I Will Survive', 1978),
+('Born to Run', 'Bruce Springsteen', 'Born to Run', 1975),
+('American Pie', 'Don McLean', 'American Pie', 1971),
+('Dancing Queen', 'ABBA', 'Arrival', 1976),
+('I Will Always Love You', 'Whitney Houston', 'The Bodyguard: Original Soundtrack Album', 1992),
+('Hallelujah', 'Leonard Cohen', 'Various Positions', 1984),
+('What a Wonderful World', 'Louis Armstrong', 'What a Wonderful World', 1967),
+('Yesterday Once More', 'The Carpenters', 'Close to You', 1970),
+('Bridge Over Troubled Water', 'Simon & Garfunkel', 'Bridge Over Troubled Water', 1970),
+("Free Fallin'", 'Tom Petty', 'Full Moon Fever', 1989),
+('I Want It That Way', 'Backstreet Boys', 'Millennium', 1999),
+("Can't Stop the Feeling!", 'Justin Timberlake', 'Trolls', 2016);
+
+select * from songs
