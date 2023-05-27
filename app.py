ae29e28f48f68dacd97a97d3daeb4302b0e793eb
kiel019 <202080497@psu.palawan.edu.ph>
2023-05-28 02:51:28 +0800

    Debugged PUT and DELETE to work prokperly

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 from flask import Flask, make_response, jsonify, request
 from flaskext.mysql import MySQL
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
 #Setting up MySQL
-mysql = MySQL(app)
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_PASSWORD'] = 'apzx0dfd6'
 app.config['MYSQL_DB'] = 'songlist'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+mysql = MySQL(app)
 
 #Root directory
 @app.route('/')
@@ -51,26 +52,35 @@ def add_song():
     cur.close()
     return make_response(jsonify({"message": "Song added successfully"}), 201)
 
+#Use PUT method to update a song in database
 @app.route("/songs/<int:id>", methods=["PUT"])
-def update_song(id):
+def update_song_by_id(id):
     cur = mysql.connection.cursor()
     json = request.get_json()
     title = json["title"]
     artist = json["artist"]
     album = json["album"]
     release_year = json["release_year"]
-    cur.execute(""" UPDATE songs SET title = %s, artist = %s, album = %s, release_year = %s WHERE id = %s""", (title, artist, album, release_year, id))
+    sqlQuery = " UPDATE songs SET title = %s, artist = %s, album = %s, release_year = %s WHERE id = %s"
+    bindData = (title, artist, album, release_year, id)
+    cur.execute(sqlQuery, bindData)
     mysql.connection.commit()
     cur.close()
-    return make_response(jsonify({"message": "song updated successfully"}), 200)
+    return make_response(jsonify({"message": "song updated successfully"}), 204)
 
+#Use DELETE method to remove a song in the database
 @app.route("/songs/<int:id>", methods=["DELETE"])
 def delete_song(id):
     cur = mysql.connection.cursor()
-    cur.execute(""" DELETE FROM songs WHERE id = %s""", (id))
+    cur.execute(""" DELETE FROM songs WHERE id = %s""", (id,))
     mysql.connection.commit()
     cur.close()
     return make_response(jsonify({"message": "song deleted successfully"}), 200)
+
+@app.route("/songs/format", methods=["GET"])
+def get_params():
+    fmt = request.args.get('id')
+    return fmt, make_response(jsonify({"format": fmt}),200)
 
 #Run the python file
 if __name__ == "__main__":
